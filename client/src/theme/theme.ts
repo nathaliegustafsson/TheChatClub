001cd23be704ebdc5ab8a5a57bee15cea9e12b8c
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-04 14:20:03 +0200

    added mui theme

---
+++ client/src/theme/theme.ts
@@ -0,0 +1,63 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#FFEAFA",
+      dark: "#FFFFFF",
+    },
+    background: {
+      default: "#08162C",
+      paper: "#FFFFFF",
+    },
+    secondary: {
+      main: "#CDDDF4",
+    },
+    text: {
+      secondary: "#ffffff",
+    },
+  },
+  typography: {
+    button: {
+      fontFamily: "Forum",
+    },
+    body2: {
+      fontFamily: "Work Sans",
+    },
+    body1: {
+      fontFamily: "Mrs Saint Delafield",
+    },
+    subtitle2: {
+      fontFamily: "Work Sans",
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontFamily: "Forum",
+    },
+    h6: {
+      fontFamily: "Forum",
+    },
+    h5: {
+      fontFamily: "Forum",
+    },
+    h4: {
+      fontFamily: "Forum",
+    },
+    h3: {
+      fontFamily: "Forum",
+    },
+    h2: {
+      fontFamily: "Forum",
+    },
+    h1: {
+      fontFamily: "Forum",
+    },
+    caption: {
+      fontFamily: "Work Sans",
+    },
+    overline: {
+      fontFamily: "Work Sans",
+    },
+  },
+});
