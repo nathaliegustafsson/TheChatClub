e10971069fa741508d82482e1842124558c7457d
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-04 14:19:17 +0200

    added routerprovider and themeprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from "@emotion/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import SocketProvider from "./context/SocketContext";
+import "./index.css";
+import StartPage from "./pages/StartPage";
+import { theme } from "./theme/theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<StartPage />} />
+      {/* <Route path="/signin" element={<SignInPage />} /> */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <SocketProvider>
+        <RouterProvider router={router} />
+      </SocketProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
