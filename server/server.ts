9a7a4245353bfc90c2b6fc3383f048c06be9ca02
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-05 13:48:46 +0200

    added more code from Davids lesson

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,36 @@ const io = new Server<
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("message", (room, message) => {
+    io.to(room).emit("message", socket.data.name!, message);
+    console.log(room, socket.data.name, message);
+  });
+
+  socket.on("join", (room, name, ack) => {
+    socket.data.name = name;
+    socket.join(room);
+    console.log(socket.rooms);
+    ack();
+    // When a user joins a room, send an updated list of rooms to everyone
+    io.emit("rooms", getRooms());
+  });
+
+  // When a new user connects, send the list of rooms
+  socket.emit("rooms", getRooms());
 });
+
+function getRooms() {
+  const { rooms } = io.sockets.adapter;
+  const roomsFound: string[] = [];
+
+  for (const [name, setOfSocketIds] of rooms) {
+    if (!setOfSocketIds.has(name)) {
+      roomsFound.push(name);
+    }
+  }
+  return roomsFound;
+}
 
 io.listen(3000);
 console.log("listening on port 3000");
