83ec1f9da1a46b06ad9022ee83ba3c10cef50af1
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-05 22:22:40 +0200

    an attempt to save username

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -12,6 +12,7 @@ interface ContextValues {
   sendMessage: (message: string) => void;
   room?: string;
   messages: Message[];
+  saveUsername: (username: string) => void;
 }
 
 const socket = io();
@@ -20,12 +21,19 @@ const SocketContext = createContext<ContextValues>(null as any);
 export const useSocket = () => useContext(SocketContext);
 
 function SocketProvider({ children }: PropsWithChildren) {
+  const [username, setUsername] = useState<string>();
   const [messages, setMessages] = useState<Message[]>([]);
   const [room, setRoom] = useState<string>();
   // const [rooms, setRooms] = useState<string>();
 
-  const joinRoom = (room: string, name: string) => {
-    socket.emit("join", room, name, () => {
+  const saveUsername = (username: string) => {
+    socket.emit("username", username, () => {
+      setUsername(username);
+    });
+  };
+
+  const joinRoom = (room: string) => {
+    socket.emit("join", room, () => {
       setRoom(room);
     });
   };
@@ -49,22 +57,29 @@ function SocketProvider({ children }: PropsWithChildren) {
     function rooms(rooms: string[]) {
       console.log(rooms);
     }
+    function username(username: string) {
+      console.log(username);
+    }
 
     socket.on("connect", connect);
     socket.on("disconnect", disconnect);
     socket.on("message", message);
     socket.on("rooms", rooms);
+    socket.on("username", username);
 
     return () => {
       socket.off("connect", connect);
       socket.off("disconnect", disconnect);
       socket.off("message", message);
       socket.off("rooms", rooms);
+      socket.off("username", username);
     };
   }, []);
 
   return (
-    <SocketContext.Provider value={{ joinRoom, sendMessage, room, messages }}>
+    <SocketContext.Provider
+      value={{ saveUsername, joinRoom, sendMessage, room, messages }}
+    >
       {children}
     </SocketContext.Provider>
   );
