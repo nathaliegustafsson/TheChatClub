48ca7beb92f478b273404ee34f8a124d0f3ff75c
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-05 12:36:16 +0200

    added route for chat page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import SocketProvider from "./context/SocketContext";
 import "./index.css";
+import ChatPage from "./pages/ChatPage";
 import StartPage from "./pages/StartPage";
 import { theme } from "./theme/theme";
 
@@ -16,7 +17,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route index element={<StartPage />} />
-      {/* <Route path="/signin" element={<SignInPage />} /> */}
+      <Route path="/chat" element={<ChatPage />} />
     </Route>
   )
 );
