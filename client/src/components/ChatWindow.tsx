628a9b4ad38c62569b9090eef2ccf206b5ba579e
Moa Hedendahl <moahedendahl@gmail.com>
2023-05-09 11:31:14 +0200

    the image is shown now, cant write anything though

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -18,57 +18,62 @@ function ChatWindow() {
         display: "flex",
         flexDirection: "column",
         height: "83vh",
-      }}
-    >
-      <h1>The Chat</h1>
-      <ul>
-        {messages.map((message, i) => (
-          <li key={i} style={{ color: "black" }}>
-            {message.username}: {message.message}
-          </li>
-        ))}
-      </ul>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "flex-end",
-          flex: 1,
-        }}
-      >
-        <form onSubmit={handlesubmit} style={{ display: "flex", flex: 1 }}>
-          <TextField
-            id="outlined-messages-input"
-            name="messages"
-            autoComplete="off"
-            placeholder="Write here..."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            // onBlur={handleBlur}
-            // error={Boolean(formik.touched.username && formik.errors.username)}
-            // helperText={formik.touched.username && formik.errors.username}
+      }}>
+      {messages.length === 0 ? (
+        <img
+          src="/src/assets/yellowmailbox.png"
+          alt="No active chat"
+          style={{ width: "100%", height: "100%", objectFit: "contain" }}
+        />
+      ) : (
+        <>
+          <h1>The Chat</h1>
+          <ul>
+            {messages.map((message, i) => (
+              <li key={i} style={{ color: "black" }}>
+                {message.username}: {message.message}
+              </li>
+            ))}
+          </ul>
+          <Box
             sx={{
+              display: "flex",
+              alignItems: "flex-end",
               flex: 1,
-              marginRight: "1rem",
-              "& .MuiInputBase-input": {
-                bgcolor: "#ECECEC",
-                borderRadius: "20rem",
-                height: "0.5rem",
-                color: (theme) => theme.palette.text.secondary,
-                fontFamily: (theme) => theme.typography.body1,
-              },
-              "& .MuiOutlinedInput-root": {
-                height: "2.5rem",
-              },
-              "& .MuiOutlinedInput-notchedOutline": {
-                borderColor: "rgb(218, 215, 215)",
-              },
-            }}
-          />
-          <Button variant="contained" type="submit">
-            Send
-          </Button>
-        </form>
-      </Box>
+            }}>
+            <form onSubmit={handlesubmit} style={{ display: "flex", flex: 1 }}>
+              <TextField
+                id="outlined-messages-input"
+                name="messages"
+                autoComplete="off"
+                placeholder="Write here..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                sx={{
+                  flex: 1,
+                  marginRight: "1rem",
+                  "& .MuiInputBase-input": {
+                    bgcolor: "#ECECEC",
+                    borderRadius: "20rem",
+                    height: "0.5rem",
+                    color: (theme) => theme.palette.text.secondary,
+                    fontFamily: (theme) => theme.typography.body1,
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    height: "2.5rem",
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "rgb(218, 215, 215)",
+                  },
+                }}
+              />
+              <Button variant="contained" type="submit">
+                Send
+              </Button>
+            </form>
+          </Box>
+        </>
+      )}
     </Container>
   );
 }
