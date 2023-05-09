a7eb8ef87c983c3cc6c9577fc252cb434b42ad10
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-09 10:01:20 +0200

    added display flex to form

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -1,41 +1,41 @@
-import { Box, Button, Container, TextField } from '@mui/material';
-import { useState } from 'react';
-import { useSocket } from '../context/SocketContext';
+import { Box, Button, Container, TextField } from "@mui/material";
+import { useState } from "react";
+import { useSocket } from "../context/SocketContext";
 
 function ChatWindow() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const { sendMessage, messages } = useSocket();
 
   const handlesubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     sendMessage(message);
-    setMessage('');
+    setMessage("");
   };
 
   return (
     <Container
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '83vh',
+        display: "flex",
+        flexDirection: "column",
+        height: "83vh",
       }}
     >
       <h1>The Chat</h1>
       <ul>
         {messages.map((message, i) => (
-          <li key={i} style={{ color: 'black' }}>
+          <li key={i} style={{ color: "black" }}>
             {message.username}: {message.message}
           </li>
         ))}
       </ul>
       <Box
         sx={{
-          display: 'flex',
-          alignItems: 'flex-end',
+          display: "flex",
+          alignItems: "flex-end",
           flex: 1,
         }}
       >
-        <form onSubmit={handlesubmit}>
+        <form onSubmit={handlesubmit} style={{ display: "flex", flex: 1 }}>
           <TextField
             id="outlined-messages-input"
             name="messages"
@@ -48,19 +48,19 @@ function ChatWindow() {
             // helperText={formik.touched.username && formik.errors.username}
             sx={{
               flex: 1,
-              marginRight: '1rem',
-              '& .MuiInputBase-input': {
-                bgcolor: '#ECECEC',
-                borderRadius: '20rem',
-                height: '0.5rem',
+              marginRight: "1rem",
+              "& .MuiInputBase-input": {
+                bgcolor: "#ECECEC",
+                borderRadius: "20rem",
+                height: "0.5rem",
                 color: (theme) => theme.palette.text.secondary,
                 fontFamily: (theme) => theme.typography.body1,
               },
-              '& .MuiOutlinedInput-root': {
-                height: '2.5rem',
+              "& .MuiOutlinedInput-root": {
+                height: "2.5rem",
               },
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'rgb(218, 215, 215)',
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(218, 215, 215)",
               },
             }}
           />
