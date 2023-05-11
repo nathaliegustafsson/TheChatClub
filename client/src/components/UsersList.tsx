2476372afd2b6e39ecc98ca227b86ffd39d3972a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-11 17:41:00 +0200

    added separate list component for users

---
+++ client/src/components/UsersList.tsx
@@ -0,0 +1,31 @@
+// import {
+//   ListItem,
+//   ListItemButton,
+//   ListItemText,
+//   Typography,
+// } from '@mui/material';
+// import * as React from 'react';
+// import { useSocket } from '../context/SocketContext';
+
+// const UserList: React.FC = () => {
+//   const { users } = useSocket();
+
+//   return (
+//     <React.Fragment>
+//       <Typography variant="h6" sx={{ ml: 3, mt: 1 }}>
+//         Users online
+//       </Typography>
+//       {users.map((user) => (
+//         <ListItem key={user.userID} sx={{ ml: 2 }}>
+//           <ListItemButton>
+//             <ListItemText>
+//               <Typography variant="body1">{user.username}</Typography>
+//             </ListItemText>
+//           </ListItemButton>
+//         </ListItem>
+//       ))}
+//     </React.Fragment>
+//   );
+// };
+
+// export default UserList;
